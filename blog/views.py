554c56e0a28c4e1c46cde9c7d14bd21b3ec27eeb
Ricardo Azeredo <ricoazeredo@yahoo.com.br>
2025-10-24 16:42:40 -0300

    update: add o olá mundo no projeto

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 from .models import Post
+
+def home(request):
+    return HttpResponse("<h1>Ola Mundo!</h1>")
 
 def post_list(request):
     post = Post.objects.order_by('-created_at')
